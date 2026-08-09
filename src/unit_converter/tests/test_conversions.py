c0acf7c10988038f6aeae722c8d63872ebba94b5
Daitan <frkng654@gmail.com>
2026-08-09 19:51:50 +0300

    Add unit converter tests

---
+++ src/unit_converter/tests/test_conversions.py
@@ -0,0 +1,25 @@
+from src.unit_converter import conversions
+
+
+def test_meter_to_millimeter():
+    assert conversions.meter_to_millimeter(2) == 2000
+
+
+def test_millimeter_to_meter():
+    assert conversions.millimeter_to_meter(2000) == 2
+
+
+def test_newton_to_kilonewton():
+    assert conversions.newton_to_kilonewton(2500) == 2.5
+
+
+def test_kilonewton_to_newton():
+    assert conversions.kilonewton_to_newton(2.5) == 2500
+
+
+def test_bar_to_pascal():
+    assert conversions.bar_to_pascal(2) == 200000
+
+
+def test_pascal_to_bar():
+    assert conversions.pascal_to_bar(200000) == 2
